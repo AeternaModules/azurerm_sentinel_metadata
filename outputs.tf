8a02e7c351ec9435318b32beeb2a9cc36ad27d75
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,89 +1,85 @@
-output "sentinel_metadatas" {
-  description = "All sentinel_metadata resources"
-  value       = azurerm_sentinel_metadata.sentinel_metadatas
-}
 output "sentinel_metadatas_author" {
-  description = "List of author values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.author]
+  description = "Map of author values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.author }
 }
 output "sentinel_metadatas_category" {
-  description = "List of category values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.category]
+  description = "Map of category values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.category }
 }
 output "sentinel_metadatas_content_id" {
-  description = "List of content_id values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.content_id]
+  description = "Map of content_id values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.content_id }
 }
 output "sentinel_metadatas_content_schema_version" {
-  description = "List of content_schema_version values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.content_schema_version]
+  description = "Map of content_schema_version values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.content_schema_version }
 }
 output "sentinel_metadatas_custom_version" {
-  description = "List of custom_version values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.custom_version]
+  description = "Map of custom_version values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.custom_version }
 }
 output "sentinel_metadatas_dependency" {
-  description = "List of dependency values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.dependency]
+  description = "Map of dependency values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.dependency }
 }
 output "sentinel_metadatas_first_publish_date" {
-  description = "List of first_publish_date values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.first_publish_date]
+  description = "Map of first_publish_date values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.first_publish_date }
 }
 output "sentinel_metadatas_icon_id" {
-  description = "List of icon_id values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.icon_id]
+  description = "Map of icon_id values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.icon_id }
 }
 output "sentinel_metadatas_kind" {
-  description = "List of kind values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.kind]
+  description = "Map of kind values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.kind }
 }
 output "sentinel_metadatas_last_publish_date" {
-  description = "List of last_publish_date values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.last_publish_date]
+  description = "Map of last_publish_date values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.last_publish_date }
 }
 output "sentinel_metadatas_name" {
-  description = "List of name values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.name]
+  description = "Map of name values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.name }
 }
 output "sentinel_metadatas_parent_id" {
-  description = "List of parent_id values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.parent_id]
+  description = "Map of parent_id values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.parent_id }
 }
 output "sentinel_metadatas_preview_images" {
-  description = "List of preview_images values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.preview_images]
+  description = "Map of preview_images values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.preview_images }
 }
 output "sentinel_metadatas_preview_images_dark" {
-  description = "List of preview_images_dark values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.preview_images_dark]
+  description = "Map of preview_images_dark values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.preview_images_dark }
 }
 output "sentinel_metadatas_providers" {
-  description = "List of providers values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.providers]
+  description = "Map of providers values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.providers }
 }
 output "sentinel_metadatas_source" {
-  description = "List of source values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.source]
+  description = "Map of source values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.source }
 }
 output "sentinel_metadatas_support" {
-  description = "List of support values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.support]
+  description = "Map of support values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.support }
 }
 output "sentinel_metadatas_threat_analysis_tactics" {
-  description = "List of threat_analysis_tactics values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.threat_analysis_tactics]
+  description = "Map of threat_analysis_tactics values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.threat_analysis_tactics }
 }
 output "sentinel_metadatas_threat_analysis_techniques" {
-  description = "List of threat_analysis_techniques values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.threat_analysis_techniques]
+  description = "Map of threat_analysis_techniques values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.threat_analysis_techniques }
 }
 output "sentinel_metadatas_version" {
-  description = "List of version values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.version]
+  description = "Map of version values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.version }
 }
 output "sentinel_metadatas_workspace_id" {
-  description = "List of workspace_id values across all sentinel_metadatas"
-  value       = [for k, v in azurerm_sentinel_metadata.sentinel_metadatas : v.workspace_id]
+  description = "Map of workspace_id values across all sentinel_metadatas, keyed the same as var.sentinel_metadatas"
+  value       = { for k, v in azurerm_sentinel_metadata.sentinel_metadatas : k => v.workspace_id }
 }
 
